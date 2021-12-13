1c03a4e9d2f23d54c8ccbd6b718933839e97c1a4
Humberto Corrêa <84969792+HumbertoCG18@users.noreply.github.com>
2021-12-13 17:55:13 -0300

    Add files via upload

---
+++ Atividades/04 Março/Aula 15.04.2021/Atividade3/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+    <title>Pagina De Registro</title>
+    
+</head>
+<body>
+<div class="container">
+    <div class="box">
+        <h1>Pagina De Registro</h1>
+    <form action="processa.php" method="post">
+    
+    <ul> 
+       <li>
+          <input type="text" name="Nome" placeholder="Nome"><br><br>     
+       </li>
+
+       <li>
+          <input type="text" name="Idade" placeholder="Idade"><br><br>     
+       </li>
+
+       <li>
+          <input type="text" name="Email" placeholder="E-mail"><br><br>      
+       </li>
+       
+       <li>
+        <input type="submit" value="Registrar">
+       </li>    
+    </ul>
+
+    </div>
+</div>
+</from>
+</body>
+</html>
+
+
+
