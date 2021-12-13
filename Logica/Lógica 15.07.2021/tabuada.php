d104d556b1bbeafc63d79ad945f16d58563df519
Humberto Corrêa <84969792+HumbertoCG18@users.noreply.github.com>
2021-12-13 18:02:59 -0300

    Add files via upload

---
+++ Logica/Lógica 15.07.2021/tabuada.php
@@ -0,0 +1,27 @@
+<?php
+
+$tabuada7 = 10;
+$tabuada8 = 10; 
+$tabuada9 = 10; 
+
+
+for ($contador = 0; $contador <= $tabuada7; $contador++) {
+  $resultado = $contador*7;
+  echo $tabuada = "7 x $contador = $resultado <br>"  ;
+}
+
+echo "------------<br>";
+
+for ($contador = 0; $contador <= $tabuada8; $contador++) {
+    $resultado = $contador*8;
+    echo $tabuada = "8 x $contador = $resultado <br>"  ;
+  }
+
+echo "------------<br>";
+
+for ($contador = 0; $contador <= $tabuada9; $contador++) {
+      $resultado = $contador*9;
+      echo $tabuada = "9 x $contador = $resultado <br>"  ;
+}
+  
+?>
